fc28360d7a8c10e0f85910e7ca48176988c2b0f4
19peterkorenak <19peterkorenak@lindberghschools.ws>
2019-02-07 12:51:30 -0600

    In class controller work

--- src/main/java/com/example/helloworld/controllers/Login.java
+++ src/main/java/com/example/helloworld/controllers/Login.java
@@ -1,4 +1,18 @@
 package com.example.helloworld.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class Login {
+    @GetMapping("/")
+    public String index() {
+        return "names/home";
+    }
+    @PostMapping("/")
+    public String loginAction() {
+        // do whatever you do to log in
+        return "names/home";
+    }
 }
