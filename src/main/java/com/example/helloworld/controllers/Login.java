2e5e18ab8662c535894cb9b9d9875ce118a516c0
Peter <19peterkorenak@lindberghschools.ws>
2019-02-09 15:34:47 -0600

    Login actually works

--- src/main/java/com/example/helloworld/controllers/Login.java
+++ src/main/java/com/example/helloworld/controllers/Login.java
@@ -1,18 +1,20 @@
 package com.example.helloworld.controllers;
 
+
 import com.example.helloworld.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping(value = "login")
 public class Login {
+    /*
     @GetMapping("/")
     public String index() {
         return "names/loginofficial";
     }
+
     @PostMapping("/")
     public String loginAction(Model model, @RequestParam("uname") String name, @RequestParam("psw") String psw) {
         // do whatever you do to log in
@@ -20,4 +22,23 @@ public class Login {
         model.addAttribute("User", user);
         return "names/home";
     }
+    */
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() {
+        System.out.println("MADE IT TO GET.");
+        return "names/loginofficial";
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String loginAction(@RequestParam("uname") String name, @RequestParam("psw") String psw) {
+        // do whatever you do to log in
+        System.out.println("Made it to post");
+        System.err.println("Username = " + name);
+        System.err.println("Password = " + psw);
+        if(name.equalsIgnoreCase("John Doe") && psw.equals("password"))
+            return "names/home";
+        else return "names/loginofficial";
+    }
 }
+
+
