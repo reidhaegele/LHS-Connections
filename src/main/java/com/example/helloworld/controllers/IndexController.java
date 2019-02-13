489aeeabb6f5b099e655ff150adf7cb41386878c
Peter <19peterkorenak@lindberghschools.ws>
2019-02-13 16:25:55 -0600

    names data

--- src/main/java/com/example/helloworld/controllers/IndexController.java
+++ src/main/java/com/example/helloworld/controllers/IndexController.java
@@ -14,16 +14,23 @@ import java.util.ArrayList;
 public class IndexController {
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String index(Model model) {
-        UserData.add(new User("John Doe", "password", "11", "butt@gmail.com", "", "", new ArrayList(0)));
+        //upperclassmen
+        UserData.add(new User("jadejade", "password", "12", "19jadewiley@lindberghschools.ws", "jade", "is cool", new ArrayList(0)));
         UserData.add(new User("benrieth19", "password", "12", "19benriethmeier@lindberghschools.ws", "Ben", "Riethmeier", new ArrayList<String>())
-        UserData.add(new User("REID_HAEGELE", "password", "12", "20reidhaegele@lindberghschools.ws", "Reid", "", new ArrayList<String>()));
-        UserData.add(new User("", "", "", "", "", "", new ArrayList<String>()));
-        UserData.add(new User("", "", "", "", "", "", new ArrayList<String>()));
-        UserData.add(new User("", "", "", "", "", "", new ArrayList<String>()));
-        UserData.add(new User("", "", "", "", "", "", new ArrayList<String>()));
-        UserData.add(new User("", "", "", "", "", "", new ArrayList<String>()));
-        UserData.add(new User("", "", "", "", "", "", new ArrayList<String>()));
-        UserData.add(new User("", "", "", "", "", "", new ArrayList<String>()));
+        UserData.add(new User("REID_HAEGELE", "password", "11", "20reidhaegele@lindberghschools.ws", "Reid", "", new ArrayList<String>()));
+        UserData.add(new User("peterkorenak", "password", "12", "19peterkorenak@lindberghschools.ws", "", "", new ArrayList<String>()));
+        UserData.add(new User("jCourson", "password", "12", "19joncourson@lindberghschools.ws", "jon", "courson", new ArrayList<String>()));
+        UserData.add(new User("ronbot", "password", "12", "19ronaldhosang@lindberghschools.ws", "Ron", "Hosang", new ArrayList<String>()));
+        UserData.add(new User("netguysam", "password", "12", "19samuelweik@lindberghschools.ws", "sam", "", new ArrayList<String>()));
+        UserData.add(new User("JoshDavis", "password", "12", "19joshuadavis@lindberghschools.ws", "Joshua", "D.", new ArrayList<String>()));
+        UserData.add(new User("misshyde", "password", "12", "19mariahyde@lindberghschools.ws", "Maria", "Hyde", new ArrayList<String>()));
+        UserData.add(new User("av5h", "password", "12", "19augusthummert@lindberghschools.ws", "auggie", "", new ArrayList<String>()));
+        //underclassmen
+        UserData.add(new User("suzyQ", "password", "9", "22susangeyer@lindberghschools.ws", "Susan", "Geyer", new ArrayList<String>()));
+        UserData.add(new User("lilMary", "if you think about it 'password' is french for 'not a sword' and that's pretty deep", "9", "22mariannelittles@lindberghschools.ws", "mary", "", new ArrayList<String>()));
+        UserData.add(new User("i_hate_math", "password", "9", "22jasonwolfard@lindberghschools.ws", "Jason", "Wolfard", new ArrayList<String>()));
+        UserData.add(new User("xJones", "password", "10", "21xavierjones@lindberghschools.ws", "X", "", new ArrayList<String>()));
+        UserData.add(new User("AnchorsAweigh", "password", "10", "21jackkorenak@lindberghschools.ws", "Jack", "K.", new ArrayList<String>()));
 
         model.addAttribute("title", "Users");
         model.addAttribute("Users", UserData.getAll());
